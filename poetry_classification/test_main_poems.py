c5d19f9c29b09e55725498855e058b844acf61bf
Gesa Johannsen <gesa.johannsen@posteo.de>
2020-03-19 17:58:06 +0100

    changed tests and created Poem-folder

--- poetry_classification/test_main_poems.py
+++ poetry_classification/test_main_poems.py
@@ -9,10 +9,10 @@ from joins_and_splits import agg_texts
 
 # test scraping
 def test_scrape_poets():
-    if os.path.exists('tests/claudius.txt'):
-        os.remove('tests/claudius.txt')
+    if os.path.exists('Poems/claudius.txt'):
+        os.remove('Poems/claudius.txt')
     scrape_texts_function(['claudius', 'sachs'])
-    assert os.path.exists('tests/claudius.txt')
+    assert os.path.exists('Poems/claudius.txt')
 
     with pytest.raises(SystemExit):
         assert scrape_texts_function(['eich', 'sachs'])
